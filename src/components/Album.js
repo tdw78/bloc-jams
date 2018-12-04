69822715238649809700dafaffbf53fa9572dcbf
Tim Weiss <tdweiss1978@gmail.com>
2018-12-04 14:59:27 -0800

    Bloc Jams Library Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,14 @@
 //Album Component
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-  <section className="album">
-    Album page will go here
-  </section>
-);
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
 
 export default Album;
