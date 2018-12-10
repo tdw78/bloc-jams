a602bcc6060b348a8854c3a87661f6297cfa91a1
Tim Weiss <tdweiss1978@gmail.com>
2018-12-10 13:42:15 -0800

    Bloc Jams Audio Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,8 +11,39 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+       this.audioElement.pause();
+       this.setState({ isPlaying: false });
+     }
+
+     setSong(song) {
+       this.audioElement.src = song.audioSrc;
+       this.setState({ currentSong: song });
+     }
+
+     handleSongClick(song) {
+       const isSameSong = this.state.currentSong === song;
+       if (this.state.isPlaying && isSameSong) {
+             this.pause();
+           } else {
+             if (!isSameSong) { this.setSong(song); } 
+             this.play();
+           }
+     }
+
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
    }
 
   render() {
@@ -35,7 +66,7 @@ class Album extends Component {
            <tbody>
            {
              this.state.album.songs.map( (song, index) =>
-                <tr key={index}>
+                <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                   <td>{album.song[index + 1]}</td>
                   <td>{album.song.title}</td>
                   <td>{album.song.duration}</td>
